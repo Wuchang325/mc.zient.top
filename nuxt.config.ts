0e8234b7e91e92d3dce37373b1fb6da8e421eebc
Wuchang325 <kxxpg666@qq.com>
2025-08-17 19:21:51 +0800

    fix: correct baseURL & prerender routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   srcDir: 'app/',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/ui'
-  ],
-  css: [
-    '@/assets/css/main.css'
-  ],
-  //ssr: true,       // 关闭服务端渲染
-  //nitro: {
-  //  prerender: {
-  //    crawlLinks: false, // 关闭自动预抓取
-  //    routes: []         // 不预渲染任何路由
-  //  }
-  //},
-  ui: {
-    fonts: false
-  },
+  modules: ['@nuxt/ui'],
+  css: ['@/assets/css/main.css'],
+  ui: { fonts: false },
   app: {
-    baseURL: 'https://mc.zient.top/',  // 设置应用的基础路径
-
+    baseURL: '/',
     head: {
-      titleTemplate: '%s | 辰砂屿'   // %s 会被当前页面标题替换
+      titleTemplate: '%s | 辰砂屿'
     }
   },
   nitro: {
@@ -33,4 +18,3 @@ export default defineNuxtConfig({
     }
   }
 })
-
